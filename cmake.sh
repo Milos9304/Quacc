e31caedbe204e9eab16bf7441e25e0c2fafe8cbb
milos_pr <milos_pr@nextgenio-login1.localdomain>
2021-03-06 17:18:09 +0000

    bug fix

--- cmake.sh
+++ cmake.sh
@@ -8,11 +8,11 @@ BUILD_TYPE=Release
 #INSTALL_DIR=$HOME/Quantum/XACC/installdir/xacc
 BUILD_TESTS=TRUE
 BUILD_EXAMPLES=TRUE
-GTEST_INCLUDE_DIR="~/.local/include/"
+QUEST_DIR="/home/nx05/nx05/milos_pr/QuEST"
 
 cmake \
   -DCMAKE_C_COMPILER=${CC} \
   -DCMAKE_CXX_COMPILER=${CXX} \
   -DCMAKE_Fortran_COMPILER=${FC} \
-  -DGTEST_INCLUDE_DIR=${GTEST_INCLUDE_DIR} \
-  ..
+  -DQUEST_DIR=${QUEST_DIR} \
+..
